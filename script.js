836f19b21586e4a7ada550b6fc5d352ed380bfce
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-28 08:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+ const level = document.getElementById("level");
+  level.addEventListener("click",levelfun);
+
+  function levelfun(){
+   
+        alert ("The level of the element is: "+ level.innerText);
+  }
+      
